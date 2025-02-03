9454030a5adc206cae072133bb8fad2ba2e27d9f
yanzhen huang <huangyanzhen0108@163.com>
2025-02-03 17:21:32 +0800

    :bee: Enhance responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // Shadcn
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { columns, DataTable } from "./repo-table";
+// import { columns, DataTable } from "./repo-table";
 
 import {
   Card,
@@ -9,6 +9,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+// Icons
+import { GitBranchIcon } from "hugeicons-react";
 
 // React
 import Link from "next/link";
@@ -36,8 +47,8 @@ const personalInfo = {
   ],
   intro: `Currently a computer science Year-4 student in University of 
   Macau. Pay interest in full-stack development and Machine learning. 
-  Working on a posture-recognition + object detection project for
-  cell phone usage detection.
+  Working on a posture recognition + object detection project for
+  cell phone usage detection. Stay tuned!
   `,
 };
 
@@ -68,15 +79,15 @@ export default async function Home() {
   return (
     <div
       className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 
-                  pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+                  pb-20 max-sm:p-4 gap-16 font-[family-name:var(--font-geist-sans)]"
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
         {/* Names & Info */}
         <div className="flex flex-row w-full items-center justify-between">
           {/* Basic Info */}
           <div>
             <p className="font-bold text-[2rem]">{personalInfo.name}</p>
-            <div className="flex flex-row w-full gap-3">
+            <div className="flex min-md:flex-row max-sm:flex-col w-full gap-3">
               <Link href={`mailto:${personalInfo.email.work[0]}`}>
                 {`Email: ${personalInfo.email.work[0]}`}
               </Link>
@@ -91,47 +102,88 @@ export default async function Home() {
           </Avatar>
         </div>
 
-        {/** Github stats */}
-        <div className="flex flex-row gap-2">
+        {/** Panels */}
+        <div
+          className="flex gap-2 max-md:flex-col flex-row "
+        // className="flex gap-2 mx-auto flex-col "
+        >
           {/** Left Panel */}
-          <div className="flex flex-row w-[50%]">
+          <div
+            className="flex flex-col md:w-[50%] sm:w-[100%] gap-2">
             <Card >
               <CardHeader>
                 <CardTitle>Github Statis</CardTitle>
                 <CardDescription>Status of GitHub Account</CardDescription>
               </CardHeader>
               <CardContent>
-                {/** Github Account Info */}
-                <div className="flex flex-row w-full gap-12 items-center">
-                  {/** Github Profile */}
-                  <div className="flex flex-col justify-left">
-                    <p className="font-bold text-[1.5rem]">
-                      {githubUserData.login || "NULL"}
-                    </p>
-                    <p className="opacity-50">{`# ${githubUserData.id}`}</p>
-                    <p className="text-[0.8rem] text-left">
-                      {githubUserData.bio}
-                    </p>
+                <div className="flex flex-col gap-2">
+                  {/** Github Account Info */}
+                  <div className="flex flex-row w-full gap-12 items-center">
+                    {/** Github Profile */}
+                    <div className="flex flex-col justify-left">
+                      <p className="font-bold text-[1.5rem]">
+                        {githubUserData.login || "NULL"}
+                      </p>
+                      <p className="opacity-50">{`# ${githubUserData.id}`}</p>
+                      <p className="text-[0.8rem] text-left">
+                        {githubUserData.bio}
+                      </p>
+                    </div>
+
+                    {/* Github Avatar */}
+                    <Avatar>
+                      <AvatarImage src={githubUserData.avatar_url} />
+                      <AvatarFallback>YZ</AvatarFallback>
+                    </Avatar>
                   </div>
 
-                  {/* Github Avatar */}
-                  <Avatar>
-                    <AvatarImage src={githubUserData.avatar_url} />
-                    <AvatarFallback>YZ</AvatarFallback>
-                  </Avatar>
-                </div>
+                  {/** Repositories */}
+                  <div className="flex flex-col items-center gap-2 justify-center">
+                    <CardTitle className="w-full p-1">Personal Repos</CardTitle>
+                    <Carousel
+                      className="flex flex-col rounded-md border w-[17.5rem] max-md:w-[15rem] max-sm:w-[14rem]"
+                      opts={{
+                        align: "start",
+                        loop: true,
+                      }}>
+                      <CarouselContent className="">
+                        {reposData.map((repo, id) => (
+                          <CarouselItem key={id} className="relative">
+                            <div className="flex flex-col gap-2 p-3 h-full">
+                              <div className="flex flex-row items-center justify-between">
+                                <div className="flex flex-row items-center justify-start gap-2">
+                                  <CardTitle className="text-[#0969da]">{repo.name}</CardTitle>
+                                  {repo.fork && (<GitBranchIcon size={15} color={"#0969da"} />)}
+                                </div>
+                                <CardDescription> {repo.language} </CardDescription>
+                              </div>
+                              {repo.description ? (
+                                <p>
+                                  {`${repo.description?.slice(0,
+                                    /[\u4E00-\u9FFF]/.test(repo.description) ? 100 : 120
+                                  )} ${repo.description?.length > (/[\u4E00-\u9FFF]/.test(repo.description) ? 100 : 120) ?
+                                    "..." : ""
+                                    }
+                                `}
+                                </p>
+                              ) : (
+                                <p className="opacity-50 italic">
+                                  No Description
+                                </p>
+                              )}
+                              <div className="absolute bottom-2 left-6 bg-white">
+                                <div className="text-sm py-1 px-2 rounded-full border">
+                                  {repo.private ? "Private" : "Public"}
+                                </div>
+                              </div>
+                            </div>
 
-                {/** Repositories */}
-                <div>
-                  <div className="flex flex-row w-full items-center justify-top py-6">
-                    <p className="font-bold text-[1.2rem]">Personal Repos</p>
-                  </div>
-                  <div className="container mx-auto">
-                    {reposData != null ? (
-                      <DataTable columns={columns} data={reposData} />
-                    ) : (
-                      <p>Failed to fetch repositories.</p>
-                    )}
+                          </CarouselItem>
+                        ))}
+                      </CarouselContent>
+                      <CarouselPrevious />
+                      <CarouselNext />
+                    </Carousel>
                   </div>
                 </div>
               </CardContent>
@@ -139,12 +191,12 @@ export default async function Home() {
           </div>
 
           {/** Right Panel */}
-          <div className="flex flex-col w-[50%] gap-2">
+          <div className="flex flex-col md:w-[50%] sm:w-[100%] gap-2">
             {/** Intorduction */}
             <Card className="h-full">
               <CardHeader>
                 <CardTitle>Introduction</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardDescription>Brief self-introduction</CardDescription>
               </CardHeader>
               <CardContent>
                 <div className="flex flex-col h-full items-center justify-top">
