f9f6ec738c5ed6d5a3f6f4a8200c41724423a8cc
Yanzhen Huang <yanzhenhuangwork@gmail.com>
2025-07-28 13:00:52 +0800

    feat: Add LLM UI

--- contents/LLM.tsx
+++ contents/LLM.tsx
@@ -1,10 +1,31 @@
 "use client"
-import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import exp from "constants";
+import { useState } from "react";
+
+export type Chat = {
+    content: string,
+    role: "user" | "assistant",
+}
 
 export default function LLM() {
+    const [chatList, setChatList] = useState<Chat[]>([
+        {'role': 'user', 'content': 'HiHiHi HiHiHiHiHiHiHiH iHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHiHi'}
+    ]);
     return (
-        <div>
-            <Textarea placeholder="LLM" />
+        <div className={`flex flex-col gap-2 w-full`}>
+            <div className={`flex flex-col w-full overflow-hidden`}>
+                {chatList.map(chat => (
+                    <p className={`w-full overflow-break-words`}>
+                        {chat.content}
+                    </p>
+                ))}
+            </div>
+            <div className={`flex flex-row items-center gap-2`}>
+                <Textarea placeholder="LLM" className={`resize-none`}/> 
+                <Button>发送</Button>
+            </div>
         </div>
     );
 }
